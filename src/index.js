415f5058e78d7e02bc32cd011076db5c07511ba1
Madeline <moon.madeline@gmail.com>
2023-06-09 14:54:39 -0500

    Process JSON data

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 // import getData from './dataFetcher.js';
 
+//Fetch weather data
 const getData = (() => {
     const fetchWeatherData = async function(city) {
         try {
-            console.log(city);
-            console.log(`https://api.weatherapi.com/v1/current.json?key=2924c3986bf946cc828152529231805&q=${city}`);
             const weather = await fetch(`https://api.weatherapi.com/v1/current.json?key=2924c3986bf946cc828152529231805&q=${city}`, {mode: 'cors'});
-            console.log(weather);
-            return weather
+            return weather.json()
+
         } catch (err) {
             console.log(err)
         }
     }
-
     return {fetchWeatherData}
 })();
 
 // export default getData;
 
-getData.fetchWeatherData('london');
+//process JSON data from API and return object with only data required for app
+const jsonProcessor = (() => {
+    const processWeatherData = async function(city) {
+        try {
+            let result = await getData.fetchWeatherData(`${city}`);
+            console.log(result);
+            return result
+        } catch (err) {
+            console.log(err);
+        }
+        //TODO: drill down to find out how to transform
+    }
+    return {processWeatherData}
+})();
+
+jsonProcessor.processWeatherData('london');
