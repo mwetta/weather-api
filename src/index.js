45d3a4534b99a923b6291b7afb3994381be1ed86
Madeline <moon.madeline@gmail.com>
2023-06-13 18:22:16 -0500

    Add basic DOM functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import getData from './dataFetcher.js';
+// import getData from './dataFetcher.js';
 import jsonProcessor from './jsonProcessor.js';
 import './style.css'
-
+import uiController from './uiController.js';
 
 
 
@@ -9,25 +9,49 @@ import './style.css'
 
 jsonProcessor.processWeatherData('london');
 
-const uiController = (() => {
+// const uiController = (() => {
 
     
-    const writeMain = (content) => {
-        let main = document.createElement('div');
-        main.setAttribute('id', 'main');
-        content.appendChild(main);
-        writeDefault();
+//     const writeMain = (content) => {
+//         let main = document.createElement('div');
+//         main.setAttribute('id', 'main');
+//         content.appendChild(main);
+//         writeDefault();
+//     }
+
+//     const clearMain = () => {
+//         let main = document.getElementById('main');
+//             while (main.firstChild) {
+//                 main.removeChild(main.firstChild);
+//             } 
+//     }
+
+//     const writeForm = () => {
+
+//     }
+
+// })();
+
+
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("new-location-search-icon");
+const span = document.getElementsByClassName("close")[0];
+const newLocation = document.getElementById("new-location");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
+  }
 
-    const clearMain = () => {
-        let main = document.getElementById('main');
-            while (main.firstChild) {
-                main.removeChild(main.firstChild);
-            } 
-    }
-
-    const writeForm = () => {
-
-    }
-
-})();
+  newLocation.addEventListener('click', function () {
+    //print new weather data
+  })
